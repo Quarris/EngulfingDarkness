dbb979a0f8ae603a9db449533aa23d475ba145f9
Quarris <quarris@live.co.uk>
2022-07-04 23:24:29 +0100

    Fixed up registry events for 1.19

--- src/main/java/dev/quarris/engulfingdarkness/RegistryEvents.java
+++ src/main/java/dev/quarris/engulfingdarkness/RegistryEvents.java
@@ -1,25 +1,25 @@
 package dev.quarris.engulfingdarkness;
 
 import dev.quarris.engulfingdarkness.capability.IDarkness;
-import net.minecraft.world.effect.MobEffect;
 import net.minecraft.world.effect.MobEffectInstance;
 import net.minecraft.world.item.alchemy.Potion;
 import net.minecraftforge.common.capabilities.RegisterCapabilitiesEvent;
-import net.minecraftforge.event.RegistryEvent;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
 import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.registries.ForgeRegistries;
+import net.minecraftforge.registries.RegisterEvent;
 
 @Mod.EventBusSubscriber(modid = ModRef.ID, bus = Mod.EventBusSubscriber.Bus.MOD)
 public class RegistryEvents {
 
     @SubscribeEvent
-    public static void registerEffect(RegistryEvent.Register<MobEffect> event) {
-        event.getRegistry().register(EngulfingDarkness.veiledMobEffect = new VeiledMobEffect().setRegistryName(ModRef.res("veiled")));
+    public static void registerEffect(RegisterEvent event) {
+        event.register(ForgeRegistries.MOB_EFFECTS.getRegistryKey(), ModRef.res("veiled"), () -> EngulfingDarkness.veiledMobEffect = new VeiledMobEffect());
     }
 
     @SubscribeEvent
-    public static void registerPotion(RegistryEvent.Register<Potion> event) {
-        event.getRegistry().register(new Potion(new MobEffectInstance(EngulfingDarkness.veiledMobEffect, 6000)).setRegistryName(ModRef.res("veiled")));
+    public static void registerPotion(RegisterEvent event) {
+        event.register(ForgeRegistries.POTIONS.getRegistryKey(), ModRef.res("veiled"), () -> new Potion(new MobEffectInstance(EngulfingDarkness.veiledMobEffect, 6000)));
     }
 
     @SubscribeEvent
